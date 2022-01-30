c4a14cb0b133efd0b21de1b703237407880fda56
GCRoots <1007879951@qq.com>
2022-01-30 22:27:20 +0800

    classLoader

---
+++ src/main/java/reflection_09/classLoader/ClassLoaderDemo.java
@@ -0,0 +1,16 @@
+package reflection_09.classLoader;
+
+public class ClassLoaderDemo {
+    public static void main(String[] args) {
+        ClassLoader c=ClassLoader.getSystemClassLoader();
+        System.out.println(c);  //AppClassLoader
+
+        ClassLoader c2 = c.getParent();
+        System.out.println(c2); //ExtClassLoader
+
+        ClassLoader c3 = c2.getParent();
+        System.out.println(c3); //null
+
+    }
+
+}
